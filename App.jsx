95cf189e07d42cafdb4847d0fe0268b5f847c6d6
ashvaaeduglobal <ashvaaeduglobal@gmail.com>
2025-04-21 04:19:02 +0545

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#1e3a8a' }}>ASHVAA EDUGLOBAL</h1>
+      <p>Guiding Your Global Education Dreams</p>
+    </div>
+  );
+}
+
+export default App;
